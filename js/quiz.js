be97a72734849dedaaaa4ef631204f7db246c134
Aariz-Haque <87598569+Aariz-Haque@users.noreply.github.com>
2021-08-10 06:50:48 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -18,8 +18,8 @@ window.onload=sendApiRequest
 
 
 async function sendApiRequest() {
-    
-    let response = await fetch(`https://opentdb.com/api.php?amount=50&category=21&difficulty=easy&type=multiple`);
+   
+    let response = await fetch(`https://opentdb.com/api.php?amount=15&category=21&difficulty=easy&type=multiple`);
     console.log(response)
     let data=await response.json()
     useApiData(data)
